ba48afdcfa4523c3cac6fd1649323ee40003df0c
doggo251 <37166660+doggo251@users.noreply.github.com>
2018-11-07 16:53:11 -0800

    Update #4
    Added inputs,rotation,and a console fps counter

--- dawgine.js
+++ dawgine.js
@@ -9,22 +9,8 @@ world positional differentiation
 */
 //image refrences
 //---------------
-var gameObjects = [];
-//player class
-//syntax:
-//new Player(id,x,y,sizeX,sizeY)
-class Player{
-    constructor(a,b,c,d,e){
-        this.id = a;
-        this.x = b;
-        this.y = c;
-        this.sizeX = d;
-        this.sizeY = e;
-        this.image = null;
-        this.color = null;
-        this.gravity = null;
-    }
-}
+var gameObjects = []; //gameobjects are seen by rayscans
+var nullObjects = []; //null objects are not seen by rayscans
 //gameObject
 //syntax:
 //new var newGO = GameObject(id,x,y,posX,posY,sizeX,sizeY);
@@ -39,12 +25,18 @@ class GameObject{
         this.image = null;
         this.color = null;
         this.gravity = null;
+        this.rotation = null;
     }
 }
 function findObject(id){
     for(var i = 0; i < gameObjects.length; i++){
         if(gameObjects[i].id == id){
             return gameObjects[i];
+        }
+    }
+    for(var i = 0; i < nullObjects.length; i++){
+        if(nullObjects[i].id == id){
+            return nullObjects[i];
         }
     }
     return null;
@@ -68,6 +60,150 @@ function rayscan(a,b,c,d){
     }
     return false;
 }
+//input
+var input = {
+    w:false,
+    a:false,
+    s:false,
+    d:false,
+    up:false,
+    left:false,
+    down:false,
+    right:false,
+    space:false,
+    f:false,
+    shift:false,
+    one:false,
+    two:false,
+    three:false,
+    four:false,
+    five:false,
+    six:false,
+    seven:false,
+    mouse1:false
+}
+document.addEventListener('keydown', function(event) {
+    switch(event.code){
+        case "KeyW":
+            input.w = true;
+            break;
+        case "KeyA":
+            input.a = true;
+            break;
+        case "KeyS":
+            input.s = true;
+            break;
+        case "KeyD":
+            input.d = true;
+            break;
+        case "ArrowUp":
+            input.up = true;
+            break;
+        case "ArrowLeft":
+            input.left = true;
+            break;
+        case "ArrowDown":
+            input.down = true;
+            break;
+        case "ArrowRight":
+            input.right = true;
+            break;
+        case "Space":
+            input.space = true;
+            break;
+        case "KeyF":
+            input.f = true;
+            break;
+        case "ShiftLeft":
+            input.shift = true;
+            break;
+        case "Digit1":
+            input.one = true;
+            break;
+        case "Digit2":
+            input.two = true;
+            break;
+        case "Digit3":
+            input.three = true;
+            break;
+        case "Digit4":
+            input.four = true;
+            break;
+        case "Digit5":
+            input.five = true;
+            break;
+        case "Digit6":
+            input.six = true;
+            break;
+        case "Digit7":
+            input.seven = true;
+            break;
+    }
+});
+document.addEventListener('keyup', function(event) {
+    switch(event.code){
+        case "KeyW":
+            input.w = false;
+            break;
+        case "KeyA":
+            input.a = false;
+            break;
+        case "KeyS":
+            input.s = false;
+            break;
+        case "KeyD":
+            input.d = false;
+            break;
+        case "ArrowUp":
+            input.up = false;
+            break;
+        case "ArrowLeft":
+            input.left = false;
+            break;
+        case "ArrowDown":
+            input.down = false;
+            break;
+        case "ArrowRight":
+            input.right = false;
+            break;
+        case "Space":
+            input.space = false;
+            break;
+        case "KeyF":
+            input.f = false;
+            break;
+        case "ShiftLeft":
+            input.shift = false;
+            break;
+        case "Digit1":
+            input.one = false;
+            break;
+        case "Digit2":
+            input.two = false;
+            break;
+        case "Digit3":
+            input.three = false;
+            break;
+        case "Digit4":
+            input.four = false;
+            break;
+        case "Digit5":
+            input.five = false;
+            break;
+        case "Digit6":
+            input.six = false;
+            break;
+        case "Digit7":
+            input.seven = false;
+            break;
+    }
+});
+document.addEventListener('mousedown', function(event) {
+    input.mouse1 = true;
+});
+document.addEventListener('mouseup', function(event) {
+    input.mouse1 = false;
+});
 //canvas creation
 var canvasName = "myCanvas"; //replace with id of canvas within the html
 var canvas = document.getElementById(canvasName);
@@ -127,21 +263,33 @@ var fov = { //fov is only used for overview follow games
     x: 400,
     y: 200
 }
-var presetWorldImage = true; //can be set to true or false
+var presetWorldImage = false; //can be set to true or false
 var worldImage = new Image();
 worldImage.src = "http://logicsimplified.com/newgames/wp-content/uploads/2017/09/shovelknight.jpg"; //world background image replace with your own
 //the start function -  where every game object is made before the game starts
-var me; //player var for use in whole game
 function start(){
-    me = new Player("me",400,400,20,20);
-    gameObjects.push(new GameObject("pepo",600,400,30,30));
-    findObject("pepo").image = pepo;
+    gameObjects.push(new GameObject("me",400,400,10,10));
+    //create objects
+    
+    //--------------
 }
 start();
 var prevTime = Date.now();
+var totalTime = 0;
+var fps = 0;
 function runGame(){
     var delta = Date.now() - prevTime;
+    totalTime += delta;
+    fps++;
+    if(totalTime >= 100){
+        console.log(fps * 10);
+        totalTime = 0;
+        fps = 0;
+    }
     prevTime = Date.now();
+    //do stuff
+
+    //--------
     draw();
     window.requestAnimationFrame(runGame);
 }
@@ -157,28 +305,36 @@ function draw(){
             ctx.drawImage(worldImage,me.x - fov.x/2,me.y - fov.y/2,fov.x,fov.y,0,0,canvas.width,canvas.height);
         }
     }
-    if(overview || nFOverview){
-        if(me.color != null){
-            ctx.fillStyle = me.color;
-            if(nFOverview){
-                ctx.fillRect((me.x - me.sizeX/2) * scaleX,(me.y - me.sizeY/2) * scaleY,me.sizeX * scaleX,me.sizeY * scaleY);
-            }
-            else if(overview){
-                ctx.fillRect(canvas.width/2 - (me.sizeX/2) * scaleX,canvas.height/2 - (me.sizeY/2) * scaleY,me.sizeX * scaleX,me.sizeY * scaleY);
-            }
-        }
-        else if(me.image != null){
-            ctx.drawImage(me.image,(me.x - me.sizeX/2) * scaleX,(me.y - me.sizeY/2) * scaleY,me.sizeX * scaleX,me.sizeY * scaleY);
-        }
-    }
-    for(var i = 0; i < gameObjects.length; i++){
-        var tempObject = gameObjects[i];
+    for(var i = 0; i < nullObjects.length; i++){
+        var tempObject = nullObjects[i];
+        
         if(tempObject.color != null){
             ctx.fillStyle = tempObject.color;
             ctx.fillRect((tempObject.x - tempObject.sizeX/2) * scaleX,(tempObject.y - tempObject.sizeY/2) * scaleY,tempObject.sizeX * scaleX,tempObject.sizeY * scaleY);
         }
         else if(tempObject.image != null){
             ctx.drawImage(tempObject.image,(tempObject.x - tempObject.sizeX/2) * scaleX,(tempObject.y - tempObject.sizeY/2) * scaleY,tempObject.sizeX * scaleX,tempObject.sizeY * scaleY);
+        }
+        
+    }
+    for(var i = 0; i < gameObjects.length; i++){
+        var tempObject = gameObjects[i];
+        if(tempObject.rotation != null){
+            ctx.translate(tempObject.x * scaleX,tempObject.y * scaleY);
+            ctx.rotate(tempObject.rotation);
+            ctx.translate(-tempObject.x * scaleX,-tempObject.y * scaleY);
+        }
+        if(tempObject.color != null){
+            ctx.fillStyle = tempObject.color;
+            ctx.fillRect((tempObject.x - tempObject.sizeX/2) * scaleX,(tempObject.y - tempObject.sizeY/2) * scaleY,tempObject.sizeX * scaleX,tempObject.sizeY * scaleY);
+        }
+        else if(tempObject.image != null){
+            ctx.drawImage(tempObject.image,(tempObject.x - tempObject.sizeX/2) * scaleX,(tempObject.y - tempObject.sizeY/2) * scaleY,tempObject.sizeX * scaleX,tempObject.sizeY * scaleY);
+        }
+        if(tempObject.rotation != null){
+            ctx.translate(tempObject.x * scaleX,tempObject.y * scaleY);
+            ctx.rotate(-tempObject.rotation);
+            ctx.translate(-tempObject.x * scaleX,-tempObject.y * scaleY);
         }
     }
 }
